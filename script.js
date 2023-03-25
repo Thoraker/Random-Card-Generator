a88c925ccda7e726f52f5bf6f8a8b777e999b941
Felix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2023-03-25 11:06:21 -0300

    Optimización del código

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 let ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
 let hearts = document.createElement("i");
-hearts.classList = "bi bi-suit-heart-fill";
-hearts.style = "color: red";
+hearts.classList = "bi bi-suit-heart-fill text-danger";
 
 let spades = document.createElement("i");
 spades.classList = "bi bi-suit-spade-fill";
 
 let clubs = document.createElement("i");
 clubs.classList = "bi bi-suit-club-fill";
-console.log(hearts);
 
 let diamonds = document.createElement("i");
-diamonds.classList = "bi bi-suit-diamond-fill";
-diamonds.style = "color: red";
+diamonds.classList = "bi bi-suit-diamond-fill text-danger";
 
 let suits = [hearts, spades, clubs, diamonds];
 
@@ -24,12 +21,9 @@ window.onload = cardGenerator = () => {
 	let suit = Math.floor(Math.random() * suits.length);
 	let cardHeader = document.createElement("div");
 	let cardFooter = document.createElement("div");
-	let cardDraw = document.createElement("div");
-	cardDraw.append(suits[suit]);
 	cardHeader.classList = "text-start";
-	cardDraw.classList = "text-center";
 	cardFooter.classList = "text-end";
 	cardHeader.textContent = ranks[rank];
 	cardFooter.textContent = ranks[rank];
-	cardContent.append(cardHeader, cardDraw, cardFooter);
+	cardContent.append(cardHeader, suits[suit], cardFooter);
 };
