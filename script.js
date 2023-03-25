c0f2f4b9b5f4ba3f627dbfb9860b9a20f8cf84fe
Felix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2023-03-25 11:32:16 -0300

    Comentarios para revisión de código

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
+// Arreglo para el valor de la carta
 let ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
+// Elementos para el palo de cada carta
 let hearts = document.createElement("i");
 hearts.classList = "bi bi-suit-heart-fill text-danger";
-
 let spades = document.createElement("i");
 spades.classList = "bi bi-suit-spade-fill";
-
 let clubs = document.createElement("i");
 clubs.classList = "bi bi-suit-club-fill";
-
 let diamonds = document.createElement("i");
 diamonds.classList = "bi bi-suit-diamond-fill text-danger";
 
-let suits = [hearts, spades, clubs, diamonds];
+let suits = [hearts, spades, clubs, diamonds]; // Arreglo con los elementos correspondientes a cada palo
 
-let cardContent = document.querySelector(".card-body");
+let cardContent = document.querySelector(".card-body"); // Selector para cargar la carta generada
 
-window.onload = cardGenerator = () => {
-	let rank = Math.floor(Math.random() * ranks.length);
+// Función para generar carta
+const cardGenerator = () => {
+    let rank = Math.floor(Math.random() * ranks.length);
 	let suit = Math.floor(Math.random() * suits.length);
 	let cardHeader = document.createElement("div");
 	let cardFooter = document.createElement("div");
 	cardHeader.classList = "text-start";
-	cardFooter.classList = "text-end";
+	cardFooter.classList = "text-start";
+    cardFooter.id = "foot";
 	cardHeader.textContent = ranks[rank];
 	cardFooter.textContent = ranks[rank];
 	cardContent.append(cardHeader, suits[suit], cardFooter);
 };
+
+window.onload = cardGenerator;
