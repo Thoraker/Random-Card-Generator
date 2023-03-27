e7a5d1c5f5e6e2d25612f42c8a19cf26de037b58
Felix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2023-03-27 12:34:06 -0300

    Se optimizan funciones

--- script.js
+++ script.js
@@ -3,76 +3,70 @@ let ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
 // Elementos para el palo de cada carta
 let hearts = document.createElement("i");
-hearts.classList = "bi bi-suit-heart-fill text-danger";
+hearts.classList =
+	"bi bi-suit-heart-fill position-absolute top-50 start-50 translate-middle";
 let spades = document.createElement("i");
-spades.classList = "bi bi-suit-spade-fill";
+spades.classList =
+	"bi bi-suit-spade-fill position-absolute top-50 start-50 translate-middle";
 let clubs = document.createElement("i");
-clubs.classList = "bi bi-suit-club-fill";
+clubs.classList =
+	"bi bi-suit-club-fill position-absolute top-50 start-50 translate-middle";
 let diamonds = document.createElement("i");
-diamonds.classList = "bi bi-suit-diamond-fill text-danger";
+diamonds.classList =
+	"bi bi-suit-diamond-fill text-danger position-absolute top-50 start-50 translate-middle";
 
 let suits = [hearts, spades, clubs, diamonds]; // Arreglo con los elementos correspondientes a cada palo
 
-// Selectores para mostar cartas en HTML
-let cardContent = document.querySelector(".card-body"); 
-let deck = document.querySelector("#deck")
-let down = document.querySelector("#down")
+// Selectores para mostrar cartas en HTML
+let cardContent = document.querySelector("#card");
+let deck = document.querySelector("#deck");
+let down = document.querySelector("#down");
+let cardHeader = document.createElement("div");
+let cardFooter = document.createElement("div");
+cardHeader.classList = "p-4 position-absolute top-0 start-0";
+cardFooter.classList = "p-4 position-absolute bottom-0 end-0";
+cardFooter.id = "foot";
 
-let downCard = []; // Arreglo con cartas boca abajo
+// Arreglo con cartas que ya aparecieron
+let downCard = [];
 
-// Función para generar carta aleatoria
+// Función para generar carta aleatoria que registra en downCard las cartas que ya aparecieron
 let cardGenerator = () => {
 	let rank = Math.floor(Math.random() * ranks.length);
 	let suit = Math.floor(Math.random() * suits.length);
 	let card = [rank, suit];
-    console.log(card);
-    return card;
+	let cardName = rank + "_" + suit;
+	if (!downCard.includes(cardName)) {
+		downCard.push(cardName);
+		console.log(card);
+		console.log(cardName);
+		console.log(downCard);
+		if (downCard.length == 1) down.style.visibility = "hidden";
+		else down.style.visibility = "";
+		if (downCard.length == 52) deck.style.visibility = "hidden";
+		else deck.style.visibility = "";
+		return card;
+	} else cardGenerator();
 };
 
+// Función para mostrar barajas y carta boca arriba
 let visibleCard = ([a, b]) => {
-    let cardName = a + "_" + b;
-    console.log(cardName);
-	if (downCard.length > 51)
-		alert("No quedan cartas en la baraja, favor revuelve los naipes");
-	else if (!downCard.includes(cardName)) {
-		downCard.push(cardName);
-        console.log(downCard);
-		while (cardContent.firstChild) {
-			cardContent.removeChild(cardContent.lastChild);
-		}
-		let cardHeader = document.createElement("div");
-		let cardFooter = document.createElement("div");
-		cardHeader.classList = "text-start";
-		cardFooter.classList = "text-start";
-		cardFooter.id = "foot";
-		cardHeader.textContent = ranks[a];
-		cardFooter.textContent = ranks[a];
-		cardContent.append(cardHeader, suits[b], cardFooter);
-        if (downCard.length == 1) {
-            down.style.visibility = "hidden";
-        }
-        else down.style.visibility = "";
-        if (downCard.length == 52) {
-            deck.style.visibility = "hidden";
-        }
-        else deck.style.visibility = "";
-	}
-    else visibleCard(cardGenerator());
+	while (cardContent.firstChild)
+		cardContent.removeChild(cardContent.lastChild);
+	cardHeader.textContent = ranks[a];
+	cardFooter.textContent = ranks[a];
+	cardContent.append(cardHeader, suits[b], cardFooter);
 };
 
 window.onload = visibleCard(cardGenerator());
 
-document.getElementById("newCard").addEventListener(
-	"click",
-	(draw = () => {
-		visibleCard(cardGenerator());
-	})
-);
+document.getElementById("newCard").addEventListener("click", newCard => {
+	if (downCard.length > 51)
+		alert("No quedan cartas en la baraja, favor revuelve los naipes");
+	else visibleCard(cardGenerator());
+});
 
-document.getElementById("newDeck").addEventListener(
-	"click",
-	(shuffle = () => {
-        downCard = [];
-		visibleCard(cardGenerator());
-	})
-);
+document.getElementById("newDeck").addEventListener("click", shuffle => {
+	downCard = [];
+	visibleCard(cardGenerator());
+});
